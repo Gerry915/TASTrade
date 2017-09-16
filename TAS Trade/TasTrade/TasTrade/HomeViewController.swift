83587ea2beb947c16ff015cdba221d9c9ec5a5d9
Gerry <gao88915@gmail.com>
2017-09-16 17:15:14 +1000

    bbb

--- TAS Trade/TasTrade/TasTrade/HomeViewController.swift
+++ TAS Trade/TasTrade/TasTrade/HomeViewController.swift
@@ -19,6 +19,10 @@ class HomeViewController: UIViewController {
         //sadjlkasdj
         //jsahahahahhaha
         //Gerry
+        
+        
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
