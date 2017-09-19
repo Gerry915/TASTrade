52d31d43c2dae888d391752fad0d5120f288a91d
Lan David <David@macbook-pro-2.local>
2017-09-19 13:20:34 +1000

    scrollbar

--- TAS Trade/TasTrade/TasTrade/HomeViewController.swift
+++ TAS Trade/TasTrade/TasTrade/HomeViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class HomeViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate, UIScrollViewDelegate {
     @IBOutlet weak var HomeCollectionView: UICollectionView!
     @IBOutlet weak var HomeScrollView: UIScrollView!
+
+    private var imageArray = [#imageLiteral(resourceName: "1"),#imageLiteral(resourceName: "DSC_0334")]
+    
+    
     
     // also enter this string as the cell identifier in the storyboard
     let reuseIdentifier = "cell"
@@ -21,6 +25,17 @@ class HomeViewController: UIViewController,UICollectionViewDataSource,UICollecti
         super.viewDidLoad()
         HomeCollectionView.delegate = self
         HomeCollectionView.dataSource = self
+        
+        //set up Advertisment scrollbar
+        for i in 0..<self.imageArray.count {
+            let xPosition = self.view.frame.size.width * CGFloat(i)
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = self.imageArray[i]
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.HomeScrollView.frame.width, height: self.HomeScrollView.frame.height)
+            self.HomeScrollView.contentSize.width = self.HomeScrollView.frame.width * CGFloat(i + 1)
+            self.HomeScrollView.addSubview(imageView)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,8 +51,6 @@ class HomeViewController: UIViewController,UICollectionViewDataSource,UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        //collectionView.register(HomeCollectionViewCell, forCellWithReuseIdentifier: "cell")
-        // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! HomeCollectionViewCell
         cell.layer.cornerRadius = 8
         cell.categoryButton.setTitle(categories[indexPath.item], for: .normal)
